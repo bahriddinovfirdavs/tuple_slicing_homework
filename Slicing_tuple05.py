2fdccb95fcf3567a08311552eb8fc820a8da483c
bahriddinovfirdavs <bahridinovfirdavs017@gmail.com>
2023-11-30 09:16:07 +0500

    add solution

--- Slicing_tuple05.py
+++ Slicing_tuple05.py
@@ -8,4 +8,8 @@ def main(tuple1,n,k):
     Returns:
         tuple: return answer.
     """
-    return tuple1[n:k+1]
+    return tuple1[n:k]
+tuple1=['a', 'b', 'c', 'd', 'e', 'f']
+n = 2
+k = 5 
+print(main(tuple1,n,k))
